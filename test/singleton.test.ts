4b14fb2b1f198a306a280c11cf4f19f42738f394
Yevhen Datsenko <eugene.datsenko.io@gmail.com>
2022-02-06 17:31:46 +0200

    fix name

--- test/singleton.test.ts
+++ test/singleton.test.ts
@@ -1,15 +1,15 @@
-import Singletone from '../src/creational/singletone/singleton';
+import Singleton from '../src/creational/singleton/singleton';
 
 describe('singleton', () => {
   test(`singleton1.timeStamp === singleton2.timeStamp`, () => {
-    const singleton1 = Singletone.instance;
-    const singleton2 = Singletone.instance;
+    const singleton1 = Singleton.instance;
+    const singleton2 = Singleton.instance;
     expect(singleton1.timeStamp).toEqual(singleton2.timeStamp);
   });
 
   test(`Singleton.instance === Singleton.instance`, () => {
-    const singleton1 = Singletone.instance;
-    const singleton2 = Singletone.instance;
+    const singleton1 = Singleton.instance;
+    const singleton2 = Singleton.instance;
     expect(singleton1 === singleton2).toBe(true);
   });
 });
