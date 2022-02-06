4abc3a252dd2d53307af3f70762b86caec917e64
Yevhen Datsenko <eugene.datsenko.io@gmail.com>
2022-02-06 15:56:07 +0200

    add implement prototype

---
+++ src/creational/prototype/prototype.ts
@@ -0,0 +1,23 @@
+interface Prototype {
+  clone(): Prototype;
+}
+
+export default class User implements Prototype {
+  private _name: string;
+
+  constructor(name: string) {
+    this._name = name;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName: string) {
+    this._name = newName;
+  }
+
+  clone() {
+    return new User(this._name);
+  }
+}
