92ba344c82c44e9cb487b64173a44bb8aa8e5395
Yevhen Datsenko <eugene.datsenko.io@gmail.com>
2022-02-07 17:53:41 +0200

    change  method

--- test/singleton.test.ts
+++ test/singleton.test.ts
@@ -4,7 +4,7 @@ describe('singleton', () => {
   test(`singleton1.timeStamp === singleton2.timeStamp`, () => {
     const singleton1 = Singleton.instance;
     const singleton2 = Singleton.instance;
-    expect(singleton1.timeStamp).toEqual(singleton2.timeStamp);
+    expect(singleton1.timeStamp).toBe(singleton2.timeStamp);
   });
 
   test(`Singleton.instance === Singleton.instance`, () => {
